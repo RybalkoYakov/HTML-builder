2bdca80cea3ea276632de2cf3931bfa181ef3600
RybalkoYakov <RybalkoYakovv@gmail.com>
2023-05-06 20:35:34 +0300

    fix: add removedirectory function to 04

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,4 +1,4 @@
-const { mkdir, readdir, copyFile } = require('fs/promises');
+const { mkdir, readdir, copyFile, unlink, rmdir, readFile} = require('fs/promises');
 const path = require('path');
 
 (async () => {
@@ -8,15 +8,36 @@ const path = require('path');
   const pathToRead = path.join(__dirname, folderName);
   const pathToWrite = path.join(__dirname, `${folderName}-${folderMod}`);
 
-
   await mkdir(pathToWrite, {recursive: true});
 
   const files = await readdir(pathToRead, {withFileTypes: true});
+
+  await removeDirectory(pathToWrite)
 
   for (const file of files) {
     const sourceFile = path.join(pathToRead, file.name);
     const destFile = path.join(pathToWrite, file.name);
 
-    copyFile(sourceFile, destFile);
+    await copyFile(sourceFile, destFile);
   }
 })()
+
+/**
+ * Removes directory with all files and directories
+ * @param node {URL | PathLike}
+ * @returns {Promise<void>}
+ */
+async function removeDirectory(node) {
+  const files = await readdir(node, {withFileTypes: true});
+
+  for (const file of files) {
+    if (file.isFile()) {
+      await unlink(path.join(node, file.name));
+    } else {
+      await removeDirectory(path.join(node, file.name));
+      await rmdir(path.join(node, file.name));
+    }
+  }
+}
+
+module.exports = { removeDirectory }
