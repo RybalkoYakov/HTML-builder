be496e5004f99fb70a626ca839b6fc6e4305b122
RybalkoYakov <RybalkoYakovv@gmail.com>
2023-05-06 14:32:22 +0300

    feat: 03-files-in-foledr complete

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,21 @@
+const { readdir, stat } = require('fs/promises');
+const path = require('path');
+
+const dirToRead = path.join(__dirname, 'secret-folder');
+const options = {encoding: 'utf8', withFileTypes: true};
+
+(async function filesInFolder() {
+  const files = await readdir(dirToRead, options);
+  for (const file of files) {
+    const filePath = path.join(dirToRead, file.name);
+    const fileName = path.parse(filePath).name;
+    const fileExt = path.extname(filePath);
+
+    const fileStat = await stat(filePath);
+    const fileSizeKb = fileStat.size / 1024;
+
+    if (file.isFile()) {
+      process.stdout.write(` <${fileName}>-<${fileExt}>-<${fileSizeKb}kb>\n`);
+    }
+  }
+})()
